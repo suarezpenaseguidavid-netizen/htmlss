faf53e41fd41b4af4d3fa139f18b0f248d6ea75a
suarezpenaseguidavid-netizen <suarezpenaseguidavid@gmail.com>
2026-05-27 13:51:50 -0500

    carrito de compras

---
+++ carrito/js/carrito.js
@@ -0,0 +1,50 @@
+let carrito= []
+
+function agregar(){
+    let nombre= document.querySelector("#nombre").value
+    let cantidad= Number(document.querySelector("#cantidad").value)
+    let precio= Number(document.querySelector("#precio").value)
+
+    let producto= {
+        id:Date.now(),
+        nombre: nombre,
+        precio: precio,
+        cantidad: cantidad
+    }
+
+    carrito.push(producto)
+    mostrarCarrito()
+}
+
+function eliminarProducto(id){
+
+    carrito = carrito.filter(producto => producto.id !== id)
+
+    mostrarCarrito()
+}
+
+function mostrarCarrito(){
+    let lista= document.querySelector("#lista")
+    lista.innerHTML = ""
+    let total = 0
+
+    carrito.forEach(producto => {
+        let li= document.createElement("li")
+        li.textContent=
+        producto.nombre +
+        "- $" + producto.precio+
+        "x" + producto.cantidad
+
+        let botonEliminar= document.createElement("button")
+        botonEliminar.textContent= "Eliminar"
+
+        botonEliminar.onclick= function(){
+            eliminarProducto(producto.id)
+        }
+
+        li.appendChild(botonEliminar)
+        lista.appendChild(li)
+
+        total += producto.precio * producto.cantidad
+    })
+}
